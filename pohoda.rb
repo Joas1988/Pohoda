763c79f2b98616aaa770c69a988c1b4429608ecd
Jaroslav B”hmer <joas1988@gmail.com>
2015-06-17 02:14:31 +0200

    gitignore

--- pohoda.rb
+++ pohoda.rb
@@ -2,12 +2,24 @@ require 'rubygems'
 require 'nokogiri'
 
 command = Thread.new do
- `"C:\\Program Files (x86)\\STORMWARE\\POHODA\\Pohoda.exe" /XML "Admin" "" "C:\\Users\\joas\\RubymineProjects\\Aiesec\\xml_export.ini"`
+ `"C:\\Program Files (x86)\\STORMWARE\\POHODA\\Pohoda.exe" /XML "Admin" "password" "C:\\Users\\joas\\RubymineProjects\\Aiesec\\base\\init.ini"`
 end
 command.join
 
-Dir.glob("output/*.xml").each do |filename|
-  doc = Nokogiri::XML(File.new(filename))
-  ucetni_jednotky = doc.xpath("//acu:dataFile")
-  puts ucetni_jednotky.inner_text if ucetni_jednotky.any?
+  jednotky = File.open("ucetni_jednotky.xml")
+  doc = Nokogiri::XML(jednotky)
+  Dir.mkdir 'output' unless Dir.exist? 'output'
+  doc.xpath("//acu:itemAccountingUnit").each do |node|
+  if node
+    output_dir = "output/#{node.xpath('//typ:company').inner_html}"
+    database = node.xpath('//acu:dataFile').inner_html
+    Dir.mkdir output_dir unless Dir.exist? output_dir
+    unless File.exist?("#{output_dir}/init.ini")
+    init_file = File.new("#{output_dir}/init.ini",'w')
+    init_file << "[XML]\ninput_dir=input\ncheck_duplicity=0,1\nformat_output=0,1\ndatabase=#{database}\nresponse_dir=#{output_dir.sub(/\//,'\\')}"
+    init_file.close
+    end
+    `"C:\\Program Files (x86)\\STORMWARE\\POHODA\\Pohoda.exe" /XML "Admin" "880526" "C:\\Users\\joas\\RubymineProjects\\Aiesec\\#{output_dir}\\init.ini"`
+  end
 end
+jednotky.close
